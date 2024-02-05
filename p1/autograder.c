eee6678539e8d47f7d8e754d52829572e5975b48
Jacob <74641690+jacobschwantes@users.noreply.github.com>
2024-02-04 21:53:10 -0600

    batch by parameter instad of executable

--- p1/autograder.c
+++ p1/autograder.c
@@ -14,38 +14,43 @@ int main(int argc, char *argv[])
 {
     const int B = atoi(argv[1]);
     char outputs[B][MAX_LENGTH];
-    char exec_path[100];
-    FILE *f;
+    int params = argc - 2;
+    int params_offset = 2;
 
     for (int i = 0; i < B; ++i)
     {
         outputs[i][0] = '\0';
     }
 
-    f = fopen("submission.txt", "r");
-
-    if (f == NULL)
+    while (params_offset < argc)
     {
-        perror("Error opening file");
-        return -1;
-    };
+        char exec_path[100];
+        FILE *f;
+        f = fopen("submission.txt", "r");
 
-    while (fgets(exec_path, 100, f) != NULL)
-    {
-        exec_path[strcspn(exec_path, "\n")] = 0;
-        printf("starting exec: %s\n", exec_path);
-        int batch_size = B;
-        int params = argc - 2;
-        while (params > 0)
+        if (f == NULL)
         {
-            if (params < batch_size)
-            {
-                batch_size = params;
-            }
-            pid_t pids[batch_size];
+            perror("Error opening file");
+            return -1;
+        };
+        int batch_size = B;
 
-            for (int i = 0; i < batch_size; i++)
+        printf("starting param: %s\n", argv[params_offset]);
+        // while (fgets(exec_path, 100, f) != NULL)
+        // {
+
+        if (params < batch_size)
+        {
+            batch_size = params;
+        }
+        pid_t pids[batch_size];
+
+        for (int i = 0; i < batch_size; i++)
+        {
+            if (fgets(exec_path, 100, f) != NULL)
             {
+                exec_path[strcspn(exec_path, "\n")] = 0;
+                printf("starting exec: %s\n", exec_path);
                 if ((pids[i] = fork()) < 0)
                 {
                     perror("failed to fork");
@@ -53,174 +58,178 @@ int main(int argc, char *argv[])
                 }
                 else if (pids[i] == 0)
                 {
-                    char *args[] = {exec_path, argv[argc - params + i], (char *)0};
+                    char *args[] = {exec_path, argv[params_offset], (char *)0};
                     printf("spawning child with pid:%d\n", getpid());
                     execv(args[0], args);
                     perror("failed to execute");
                     exit(1);
                 }
             }
+        }
 
-            /* Parent code */
-            int status;
-            pid_t pid;
+        /* Parent code */
+        int status;
+        pid_t pid;
 
-            int count = 0;
-            // check every second for child status updates until slow child finishes
-            while (count <= 11)
+        int count = 0;
+        // check every second for child status updates until slow child finishes
+        while (count <= 11)
+        {
+            pid = waitpid(-1, &status, WNOHANG);
+
+            if (pid == -1)
             {
-                pid = waitpid(-1, &status, WNOHANG);
-
-                if (pid == -1)
+                /* an error other than an interrupted system call */
+                perror("waitpid");
+                return 0;
+            }
+            else if (pid != 0)
+            {
+                if (WIFEXITED(status)) /* process exited normally */
                 {
-                    /* an error other than an interrupted system call */
-                    perror("waitpid");
-                    return 0;
-                }
-                else if (pid != 0)
-                {
-                    if (WIFEXITED(status)) /* process exited normally */
-                    {
-                        for (int i = 0; i < batch_size; i++)
-                        {
-                            if (pids[i] == pid)
-                            {
-                                switch (WEXITSTATUS(status))
-                                {
-                                case 1:
-                                    appendstr(outputs, "I", i);
-                                    break;
-                                case 0:
-                                    if (count > (L * 2))
-                                    {
-                                        appendstr(outputs, "S", i);
-                                    }
-                                    else
-                                    {
-                                        appendstr(outputs, "C", i);
-                                    }
-                                    break;
-                                default:
-                                    break;
-                                }
-                            }
-                        }
-                        printf("child process %d exited with value %d\n", pid, WEXITSTATUS(status));
-                    }
-                    else if (WIFSIGNALED(status))
-                    {
-                        for (int i = 0; i < batch_size; i++)
-                        {
-                            if (pids[i] == pid)
-                            {
-                                switch (WTERMSIG(status))
-                                {
-                                case 11:
-                                    appendstr(outputs, "F", i);
-                                    break;
-                                default:
-                                    break;
-                                }
-                            }
-                        }
-                        printf("child process %d exited due to signal %d\n", pid, WTERMSIG(status));
-                    }
                     for (int i = 0; i < batch_size; i++)
                     {
                         if (pids[i] == pid)
                         {
-                            pids[i] = 0;
+                            switch (WEXITSTATUS(status))
+                            {
+                            case 1:
+                                appendstr(outputs, "I", i);
+                                break;
+                            case 0:
+                                if (count > (L * 2))
+                                {
+                                    appendstr(outputs, "S", i);
+                                }
+                                else
+                                {
+                                    appendstr(outputs, "C", i);
+                                }
+                                break;
+                            default:
+                                break;
+                            }
                         }
                     }
+                    printf("child process %d exited with value %d\n", pid, WEXITSTATUS(status));
                 }
-                else
+                else if (WIFSIGNALED(status))
                 {
-                    printf("still waiting\n");
+                    for (int i = 0; i < batch_size; i++)
+                    {
+                        if (pids[i] == pid)
+                        {
+                            switch (WTERMSIG(status))
+                            {
+                            case 11:
+                                appendstr(outputs, "F", i);
+                                break;
+                            default:
+                                break;
+                            }
+                        }
+                    }
+                    printf("child process %d exited due to signal %d\n", pid, WTERMSIG(status));
                 }
-
-                // int j = 0;
-                // while (pids[j] == 0 && j < batch_size)
-                // {
-                //     if (j == (batch_size - 1))
-                //     {
-                //         count = 11;
-                //     }
-                //     j++;
-                // }
-
-                ++count;
-                sleep(1);
+                for (int i = 0; i < batch_size; i++)
+                {
+                    if (pids[i] == pid)
+                    {
+                        pids[i] = 0;
+                    }
+                }
             }
-            // check for blocked or infinite children and kill them
-            for (int i = 0; i < batch_size; i++)
+            else
+            {
+                printf("still waiting\n");
+            }
+
+            // int j = 0;
+            // while (pids[j] == 0 && j < batch_size)
+            // {
+            //     if (j == (batch_size - 1))
+            //     {
+            //         count = 11;
+            //     }
+            //     j++;
+            // }
+
+            ++count;
+            sleep(1);
+        }
+        // check for blocked or infinite children and kill them
+        for (int i = 0; i < batch_size; i++)
+        {
+
+            if (pids[i] != 0)
             {
 
-                if (pids[i] != 0)
+                // Construct the path to the status file
+                char path[50];
+                snprintf(path, sizeof(path), "/proc/%d/status", pids[i]);
+
+                // Open the status file for reading
+                FILE *file = fopen(path, "r");
+
+                if (file == NULL)
                 {
-
-                    // Construct the path to the status file
-                    char path[50];
-                    snprintf(path, sizeof(path), "/proc/%d/status", pids[i]);
-
-                    // Open the status file for reading
-                    FILE *file = fopen(path, "r");
-
-                    if (file == NULL)
-                    {
-                        perror("Error opening file");
-                        exit(EXIT_FAILURE);
-                    }
-
-                    // Read and print the content of the file
-                    char buffer[256];
-                    while (fgets(buffer, sizeof(buffer), file) != NULL)
-                    {
-                        if (sscanf(buffer, "State:\t%c", &buffer[0]) == 1)
-                        {
-                            // printf("Process %d is in state: %c\n", pids[i], buffer[0]);
-                            break; // Stop reading after finding the state information
-                        }
-                    }
-                    fclose(file);
-
-                    if (buffer[0] == 'S')
-                    {
-                        appendstr(outputs, "B", i);
-                        printf("state is S so killing blocked child: %d\n", pids[i]);
-                    }
-                    if (buffer[0] == 'R')
-                    {
-                        appendstr(outputs, "L", i);
-                        printf("state is R so killing infinite loop child: %d\n", pids[i]);
-                    }
-
-                    kill(pids[i], SIGKILL);
-
-                    // printf("stale process: %d\n", pids[i]);
+                    perror("Error opening file");
+                    exit(EXIT_FAILURE);
                 }
-            }
-            params -= batch_size;
-        }
-    }
-    fclose(f);
-    f = fopen("submission.txt", "r");
 
+                // Read and print the content of the file
+                char buffer[256];
+                while (fgets(buffer, sizeof(buffer), file) != NULL)
+                {
+                    if (sscanf(buffer, "State:\t%c", &buffer[0]) == 1)
+                    {
+                        // printf("Process %d is in state: %c\n", pids[i], buffer[0]);
+                        break; // Stop reading after finding the state information
+                    }
+                }
+                fclose(file);
+
+                if (buffer[0] == 'S')
+                {
+                    appendstr(outputs, "B", i);
+                    printf("state is S so killing blocked child: %d\n", pids[i]);
+                }
+                if (buffer[0] == 'R')
+                {
+                    appendstr(outputs, "L", i);
+                    printf("state is R so killing infinite loop child: %d\n", pids[i]);
+                }
+
+                kill(pids[i], SIGKILL);
+
+                // printf("stale process: %d\n", pids[i]);
+            }
+        }
+        params_offset++;
+        // }
+        fclose(f);
+    }
+
+    FILE *f;
+
+    f = fopen("submission.txt", "r");
+    char exec_name[100];
     for (int i = 0; i < B; i++)
     {
-        if (fgets(exec_path, 100, f) != NULL)
+        if (fgets(exec_name, 100, f) != NULL)
         {
-            exec_path[strcspn(exec_path, "\n")] = 0;
-            const char *lastSeparator = strrchr(exec_path, '/');
+            exec_name[strcspn(exec_name, "\n")] = 0;
+            const char *lastSeparator = strrchr(exec_name, '/');
 
             if (lastSeparator != NULL)
             {
                 // The last segment is everything after the last separator
                 const char *lastSegment = lastSeparator + 1;
 
-                strcpy(exec_path, lastSegment);
+                strcpy(exec_name, lastSegment);
             }
 
-            printf("%s: ", exec_path);
+            printf("%s: ", exec_name);
             for (int j = 0; j < argc - 2; j++)
             {
 
@@ -250,7 +259,7 @@ int main(int argc, char *argv[])
                     break;
                 }
                 printf("%c %s", *argv[2 + j], code);
-                if (j != (B - 1))
+                if (j != (params - 1))
                 {
                     printf(", ");
                 }
